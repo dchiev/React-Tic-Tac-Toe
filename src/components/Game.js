aeaae0023390679e47bfdfc8d5c1a23d9a141391
Dan Chiev <danchiev2@Gmail.com>
2021-03-18 18:41:07 -0400

    added game logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -7,4 +7,34 @@ const Game = () => {
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(hisotry[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+    // select square
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+  return (
+    <>
+      <h1>React Tic Tac Toe - With Hooks</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div>
+        <div className="info-wrapper">
+          <h3>History</h3>
+          {renderMoves()}
+        </div>
+
+        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO}</h3>
+      </div>
+    </>
+  );
 };
+
+export default Game;
